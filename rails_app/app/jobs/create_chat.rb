df5b05524a1c076494449f557072037a8e852e1c
Ahmed Ali <ahmed.alawadly@gmail.com>
2021-01-30 15:52:20 +0200

    added sidekiq workers

---
+++ rails_app/app/jobs/create_chat.rb
@@ -0,0 +1,12 @@
+class CreateChat
+  include Sidekiq::Worker
+  sidekiq_options queue: 'chats', retry: false
+
+  def perform(app_token, chat_number)
+    app = App.find_by(token: app_token)
+    app.with_lock do
+      Chat.create!(app: app, number: chat_number)
+      app.update!(chats_count: app.chats_count + 1)
+    end
+  end
+end
---
+++ rails_app/app/jobs/create_message.rb
@@ -0,0 +1,13 @@
+class CreateMessage
+  include Sidekiq::Worker
+  sidekiq_options queue: 'messages', retry: false
+
+  def perform(app_token, chat_number, message_number, body)
+    app = App.find_by(token: app_token)
+    chat = app.chats.find_by(number: chat_number)
+    chat.with_lock do
+      Message.create!(chat: chat, number: message_number, body: body)
+      chat.update!(messages_count: chat.messages_count + 1)
+    end
+  end
+end
