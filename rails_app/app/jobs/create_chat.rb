d503435a7bd8b0dec6ced5944aab43cb29f63ba8
Ahmed Ali <ahmed.alawadly@gmail.com>
2021-01-30 21:31:47 +0200

    use find_by! to force jobs to fail early

--- rails_app/app/jobs/create_chat.rb
+++ rails_app/app/jobs/create_chat.rb
@@ -3,7 +3,7 @@ class CreateChat
   sidekiq_options queue: 'chats', retry: false
 
   def perform(app_token, chat_number)
-    app = App.find_by(token: app_token)
+    app = App.find_by!(token: app_token)
     app.with_lock do
       Chat.create!(app: app, number: chat_number)
       app.update!(chats_count: app.chats_count + 1)
--- rails_app/app/jobs/create_message.rb
+++ rails_app/app/jobs/create_message.rb
@@ -3,8 +3,8 @@ class CreateMessage
   sidekiq_options queue: 'messages', retry: false
 
   def perform(app_token, chat_number, message_number, body)
-    app = App.find_by(token: app_token)
-    chat = app.chats.find_by(number: chat_number)
+    app = App.find_by!(token: app_token)
+    chat = app.chats.find_by!(number: chat_number)
     chat.with_lock do
       Message.create!(chat: chat, number: message_number, body: body)
       chat.update!(messages_count: chat.messages_count + 1)
